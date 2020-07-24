aeba574f1a66fcdfd8046c2aec6a2e2e61831b04
Michael Cook <mcook4728@gmail.com>
2020-07-24 12:50:12 -0700

    update paladins for shadowlands

--- configurations/paladin.lua
+++ configurations/paladin.lua
@@ -54,5 +54,42 @@ PRD.configurations.paladin_retribution = {
             local percent = cache.currentPower / cache.maxPower
             return true, { r = r, g = g, b = b * percent }
         end
+    },
+    top = {
+        currentPower_events = { "UNIT_HEALTH_FREQUENT" },
+        currentPower = function(cache, event, ...) 
+            if event == "UNIT_HEALTH_FREQUENT" and select(1, ...) ~= "player" then
+                return false
+            end
+
+            cache.currentPower = UnitHealth("player")
+
+            return true, cache.currentPower
+        end,
+        maxPower_events = { "UNIT_MAXHEALTH" },
+        maxPower = function(cache, event, ...) 
+            if event == "UNIT_MAXHEALTH" and select(1, ...) ~= "player" then
+                return false
+            end
+
+            cache.maxPower = UnitHealthMax("player")
+
+            return true, cache.maxPower
+        end,
+        texture = "Interface\\Addons\\SharedMedia\\statusbar\\Cloud",
+        text = {
+            value_dependencies = { "currentPower", "maxPower" },
+            value = function(cache, event, ...)
+                return true, string.format("%.0f%%", (cache.currentPower / cache.maxPower) * 100)
+            end,
+            xOffset = 65,
+            yOffset = 2,
+            size = 8
+        },
+        color_dependencies = { "currentPower", "maxPower" },
+        color = function(cache, event, ...)
+            local healthRatio = cache.currentPower / cache.maxPower
+            return true, { r = 1.0 - healthRatio, g = healthRatio, b = 0.0 }
+        end
     }
 }
